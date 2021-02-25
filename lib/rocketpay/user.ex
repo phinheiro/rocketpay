40f52f9e03a27af89f705cfa8ed37fa67191ebe8
Pedro Pinheiro <phleitepinheiro@gmail.com>
2021-02-25 09:45:12 -0300

    finalizacao aula 2
    conceitos aprendidos:
    - schemas e changesets
    - facades
    - criacao de controller de usuario
    - criacao de views pra mapear resposta json

--- lib/rocketpay/user.ex
+++ lib/rocketpay/user.ex
@@ -2,6 +2,8 @@ defmodule Rocketpay.User do
   use Ecto.Schema
   import Ecto.Changeset
 
+  alias Ecto.Changeset
+
   @primary_key {:id, :binary_id, autogenerate: true}
   @required_params [:name, :age, :email, :password, :nickname]
 
@@ -12,6 +14,8 @@ defmodule Rocketpay.User do
     field :password, :string, virtual: true
     field :password_hash, :string
     field :nickname, :string
+
+    timestamps()
   end
 
   def changeset(params) do
@@ -19,14 +23,15 @@ defmodule Rocketpay.User do
     |> cast(params, @required_params)
     |> validate_required(@required_params)
     |> validate_length(:password, min: 6)
-    |> validate_number(:age, greater_then_or_equal_to: 18)
+    |> validate_number(:age, greater_than_or_equal_to: 18)
     |> validate_format(:email, ~r/@/)
     |> unique_constraint([:email])
     |> unique_constraint([:nickname])
-    |> put_password_hash()
   end
 
-  defp put_password_hash(password) do
+  # defp put_password_hash(%Changeset{valid?: true, changes: %{password: password}} = changeset) do
+  #   changeset(changeset, )
+  # end
 
-  end
+  # defp put_password_hash(changeset), do: changeset
 end
