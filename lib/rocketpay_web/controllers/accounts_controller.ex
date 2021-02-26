3d722237c7482d7c5984dbea055d06864003da91
Pedro Pinheiro <phleitepinheiro@gmail.com>
2021-02-26 17:41:01 -0300

    inicio aula 4

--- lib/rocketpay_web/controllers/accounts_controller.ex
+++ lib/rocketpay_web/controllers/accounts_controller.ex
@@ -3,10 +3,18 @@ defmodule RocketpayWeb.AccountsController do
 
   alias Rocketpay.Account
 
-  action_fallback RocketpayWeb.AccountsController
+  action_fallback RocketpayWeb.FallbackController
 
   def deposit(conn, params) do
     with {:ok, %Account{} = account} <- Rocketpay.deposit(params) do
+      conn
+      |> put_status(:ok)
+      |> render("update.json", account: account)
+    end
+  end
+
+  def withdraw(conn, params) do
+    with {:ok, %Account{} = account} <- Rocketpay.withdraw(params) do
       conn
       |> put_status(:ok)
       |> render("update.json", account: account)
